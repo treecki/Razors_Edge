8966474b1cc5d21e197290d3cd409bc5fc0ec3c3
Drew Bushnell <dmbushnell@hotmail.com>
2016-05-29 23:48:05 -0700

    Fat

--- Tile_Movement_RE/Assets/Scripts/TileType.cs
+++ Tile_Movement_RE/Assets/Scripts/TileType.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 [System.Serializable]
 public class TileType {
-
+	//Alex is fat
 	public string name;
 	public GameObject tileVisualPrefab;
 	public float movementCost = 1;
